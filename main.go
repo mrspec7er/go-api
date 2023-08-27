55c707dc65512ea11d59dc592e51395e5f829349
mrspec7er <wijayakusumasandi@gmail.com>
2023-08-27 20:22:03 +0800

    feat: 🎸 main
    added routing with ServeMux

--- main.go
+++ main.go
@@ -1,20 +1,46 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 )
 
-func greetings(w http.ResponseWriter, r *http.Request)  {
-	fmt.Fprintf(w, "Hello There!")
+func greetings(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello There!")
 }
 
-func handleRequest()  {
+func stockMarket(w http.ResponseWriter, r *http.Request) {
+	stock := []string{"BBCA", "BBNI", "GOTO"}
+	json.NewEncoder(w).Encode(stock)
+}
+
+// HandleFunc
+func handleRequestWithHandleFunc()  {
 	http.HandleFunc("/", greetings)
+	http.HandleFunc("/stocks", stockMarket)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
-func main()  {
-	handleRequest()
+//ServeMux
+func handleRequestWithServeMux()  {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", greetings)
+	mux.HandleFunc("/stocks", stockMarket)
+
+	server := http.Server{
+		Addr: ":8000",
+		Handler: mux,
+	}
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
 }
+
+func main()  {
+	// handleRequestWithHandleFunc()
+	handleRequestWithServeMux()
+}
+
